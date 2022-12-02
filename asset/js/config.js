7adcf7f3cc24995934626b7648f79f80854dc20f
Ezekiel8807 <ayeoluwaseyi@gmail.com>
2022-12-02 23:18:39 +0100

    stopwatch

--- asset/js/config.js
+++ asset/js/config.js
@@ -1,58 +1,60 @@
 var t = "";
-var s = "";
 
-var timeValue = document.querySelector("#milli_seconds");
+var miliseconds = document.querySelector("#milli_seconds");
 var seconds = document.querySelector("#seconds");
+var minutes = document.querySelector("#minutes");
+var hours = document.querySelector("#hours");
+
 
 const stopWatch = () => {
-    var mili = parseInt(timeValue.innerHTML);
 
-    if( timeValue.innerHTML == 999 ) {
-        timeValue.innerHTML = "000";
-
-    }else {
-
-        if ( mili < 10 ) {
-            timeValue.innerHTML = "00" + ++mili;
-
-        }else if ( mili < 100){
-            timeValue.innerHTML = "0" + ++mili;
-
-        }else {
-            timeValue.innerHTML = ++mili;
-        }
-        
-    }
-}
-
-//function to start seconds counting
-const startSeconds = () => {
+    var mili = parseInt(miliseconds.innerHTML);
     var sec = parseInt(seconds.innerHTML);
+    var min = parseInt(minutes.innerHTML);
+    var hrs = parseInt(hours.innerHTML);
 
-    if( sec == 60){
-        seconds.innerHTML = "00";
+    mili += 1;
 
-    }else {
+    if( mili == 60 ) {
+        mili = "00";
+        sec++;
 
-        if( sec < 10 ){
-            seconds.innerHTML = "0" + ++sec;
+        if(sec == 60){
+            sec = "00";
+            min++;
 
-        }else {
-            seconds.innerHTML = ++sec;
+
+            if( min == 60){
+                min = "00";
+                hrs++;
+            }
         }
     }
-}
 
+    let h = hrs < 10 ? "0" + hrs : hrs;
+    let m = min < 10 ? "0" + min : min;
+    let s = sec < 10 ? "0" + sec : sec;
+    let mi = mili < 10 ? "0" + mili : mili;
+
+    miliseconds.innerHTML = mi;
+    seconds.innerHTML = s;
+    minutes.innerHTML = m;
+    hours.innerHTML = h;
+}
 
 
 const start = document.querySelector("#start_button").addEventListener("click", ()=> {
-    t = setInterval(stopWatch, 60);
-    s = setInterval(startSeconds, 1000);
+    t = setInterval(stopWatch, 15);
 })
 
 
 const reset = document.querySelector("#reset_button").addEventListener("click", ()=> {
-    timeValue.innerHTML = "000";
+    clearInterval(t);
+
+    miliseconds.innerHTML = "00"
+    seconds.innerHTML = "00"
+    minutes.innerHTML = "00"
+    hours.innerHTML = "00"
 
 })
 
